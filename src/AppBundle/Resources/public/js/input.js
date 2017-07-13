290928094b1db3d9a4844a5f7ec67905285640ff
ghedger <greg@hedgersoftware.com>
2017-07-13 05:14:42 +0000

    Further debugging on URL on state change.

--- src/AppBundle/Resources/public/js/input.js
+++ src/AppBundle/Resources/public/js/input.js
@@ -1,6 +1,6 @@
+// This instantiates a keydown event listener to provide a basic user input.
 
-if (typeof KeyEvent === "undefined")
-{
+if (typeof KeyEvent === "undefined") {
     var KeyEvent = {
         DOM_VK_1: 49,
         DOM_VK_2: 50,
@@ -10,13 +10,16 @@ if (typeof KeyEvent === "undefined")
     };
 }
 
-addEventListener("keydown", function (e)
-{
+addEventListener("keydown", function (e) {
     // Range validation
-    if ( KeyEvent.DOM_VK_1 <= e.keyCode && KeyEvent.DOM_VK_5 >= e.keyCode ) {
+    if (KeyEvent.DOM_VK_1 <= e.keyCode && KeyEvent.DOM_VK_5 >= e.keyCode) {
         // Put the new URL to be the choice: keycode - minimum_possible_keycode.
         var newUrl = window.location.href;
-        newUrl = newUrl.substr(0, newUrl.lastIndexOf('/')) + '/';
+        // Determine if this is the start page or not; if so, let's not chop off rightmost segment.
+        if (!isNaN(newUrl.charAt(newUrl.lastIndexOf('/') + 1))) {
+            newUrl = newUrl.substr(0, newUrl.lastIndexOf('/'));
+        }
+        newUrl += '/';
         window.location.href = newUrl + (e.keyCode - KeyEvent.DOM_VK_1);
     }
 });
