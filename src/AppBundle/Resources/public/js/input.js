2d1bb2f997c26ee109c729d27c15e73f804c043f
ghedger <greg@hedgersoftware.com>
2017-07-13 17:56:41 +0000

    Refined documentation in input.js explaining the data flow from keypress to controller; Tweaked presentation page, adding a title and a space for statistcs.

--- src/AppBundle/Resources/public/js/input.js
+++ src/AppBundle/Resources/public/js/input.js
@@ -1,5 +1,5 @@
 // This instantiates a keydown event listener to provide a basic user input.
-
+// Some versions of Chrome lack KeyEvent VK_ definitions, so we check that here and accommodate it.
 if (typeof KeyEvent === "undefined") {
     var KeyEvent = {
         DOM_VK_1: 49,
@@ -11,15 +11,20 @@ if (typeof KeyEvent === "undefined") {
 }
 
 addEventListener("keydown", function (e) {
-    // Range validation
+    // Perform range validation against key pressed to only allow "1" - "5" keys
     if (KeyEvent.DOM_VK_1 <= e.keyCode && KeyEvent.DOM_VK_5 >= e.keyCode) {
-        // Put the new URL to be the choice: keycode - minimum_possible_keycode.
+        // Here, we are going to finagle the current URL.  While slightly complicated, this keeps us location-agnostic.
+        // Append the choice to the new URL, ergo "1" key becomes <url>/0, "4" becomes <url>/3, etc.
+        // This last segment is fed into the controller as the input parameter representing the user's choice
+        // in the game and evaluated against a random computer choice.
         var newUrl = window.location.href;
-        // Determine if this is the start page or not; if so, let's not chop off rightmost segment.
+        // Determine if this URL is the start page or not as dictated by the absence of a number as the last segment.
+        // If so, let's not chop off the rightmost segment.
         if (!isNaN(newUrl.charAt(newUrl.lastIndexOf('/') + 1))) {
             newUrl = newUrl.substr(0, newUrl.lastIndexOf('/'));
         }
         newUrl += '/';
+        // Redirect to the page which will invoke the controller.
         window.location.href = newUrl + (e.keyCode - KeyEvent.DOM_VK_1);
     }
 });
